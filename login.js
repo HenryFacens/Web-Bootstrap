075e60cd9c108091f85ee2c2d7ea1aa46a87bdda
HenryFacens <h.santuriao@gmail.com>
2022-11-08 11:28:46 -0300

    att

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ var username = document.getElementById("RA").value;
 var password = document.getElementById("senha").value;
 if ( username == "Formget" && password == "formget#123"){
 alert ("Login successfully");
-window.location = "success.html"; // Redirecting to other page.
+window.location = "index.html"; // Redirecting to other page.
 return false;
 }
 else{
